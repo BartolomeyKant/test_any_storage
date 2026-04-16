a403952ac4bae237874c35667e1f1760b4a373b9
BartolomeyKant <notevill@gmail.com>
2026-04-16 09:15:20 +0500

    add how much copied and moved, add reserve memory in vector

--- src/test_tables.h
+++ src/test_tables.h
@@ -8,13 +8,15 @@
 #include <vector>
 
 static inline std::size_t foo_destructed = 0;
+static inline std::size_t foo_copied = 0;
+static inline std::size_t foo_moved = 0;
 
 template <std::size_t I> struct Foo {
   Foo()
       : a{static_cast<int>(I)}, b{static_cast<float>(I) / 2.F},
         c{std::format("Hello {}", I)} {}
-  Foo(const Foo &f) : a{f.a}, b{f.b}, c{f.c} {}
-  Foo(Foo &&f) noexcept : a{f.a}, b{f.b}, c{std::move(f.c)} {}
+  Foo(const Foo &f) : a{f.a}, b{f.b}, c{f.c} { foo_copied++; }
+  Foo(Foo &&f) noexcept : a{f.a}, b{f.b}, c{std::move(f.c)} { foo_moved++; }
   ~Foo() {
     if (!c.empty()) { // if not moved
       foo_destructed++;
@@ -28,7 +30,7 @@ template <std::size_t I> struct Foo {
 
 template <typename Storage, std::size_t... Is>
 void fill_foo_impl(std::vector<Storage> &anys, std::index_sequence<Is...>) {
-  (anys.push_back(Foo<Is>{}), ...);
+  (anys.emplace_back(Foo<Is>{}), ...);
 }
 
 template <typename Storage, std::size_t N>
@@ -38,6 +40,7 @@ void fill_foo(std::vector<Storage> &anys) {
 
 template <typename Storage, std::size_t N> int test_storage() {
   std::vector<Storage> anys;
+  anys.reserve(N);
   fill_foo<Storage, N>(anys);
   auto anys2 = anys;
   auto anys3 = std::move(anys2);
@@ -51,9 +54,10 @@ template <typename Storage, std::size_t N> int test_storage() {
   anys2.clear();
   anys3.clear();
 
-  std::cout << std::format("foo_destructed: {}\n", foo_destructed);
+  std::cout << std::format("foo_destructed: {} foo_copied: {} foo_moved: {}\n",
+                           foo_destructed, foo_copied, foo_moved);
   // expected 2xN because anys2 was moved
-  if (foo_destructed != 2 * N) {
+  if (foo_destructed != 2 * N || foo_copied != N || foo_moved != N) {
     return 1;
   }
   return 0;
