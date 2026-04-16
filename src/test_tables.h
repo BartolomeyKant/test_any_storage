9286c15ee28ca51332e004663c4e2d9b045fb5b2
BartolomeyKant <notevill@gmail.com>
2026-04-16 09:58:58 +0500

    add fizz buzz to example

--- src/test_tables.h
+++ src/test_tables.h
@@ -11,7 +11,18 @@ static inline std::size_t foo_destructed = 0;
 static inline std::size_t foo_copied = 0;
 static inline std::size_t foo_moved = 0;
 
-template <std::size_t I> struct Foo {
+struct FooBase {
+  virtual void print() const = 0;
+
+protected:
+  ~FooBase() = default;
+};
+
+template <std::size_t I, typename _ = void> struct Foo;
+
+template <std::size_t I>
+struct Foo<I, std::enable_if_t<(I % 3 == 0) && (I % 5 == 0)>> final
+    : public FooBase {
   Foo()
       : a{static_cast<int>(I)}, b{static_cast<float>(I) / 2.F},
         c{std::format("Hello {}", I)} {}
@@ -23,9 +34,72 @@ template <std::size_t I> struct Foo {
     }
   }
 
+  void print() const override {
+    std::cout << std::format("FizzBuzz: a:{}, b:{}, c:{}\n", a, b, c);
+  }
+
   int a;
   float b;
   std::string c;
+};
+
+template <std::size_t I>
+struct Foo<I, std::enable_if_t<(I % 3 == 0) && (I % 5 != 0)>> final
+    : public FooBase {
+  Foo() : a{static_cast<int>(I)} {}
+  Foo(const Foo &f) : a{f.a} { foo_copied++; }
+  Foo(Foo &&f) noexcept : a{f.a} {
+    foo_moved++;
+    f.a = -1;
+  }
+  ~Foo() {
+    if (a != -1) { // if not moved
+      foo_destructed++;
+    }
+  }
+
+  void print() const override { std::cout << std::format("Fizz: a:{}\n", a); }
+
+  int a;
+};
+
+template <std::size_t I>
+struct Foo<I, std::enable_if_t<(I % 3 != 0) && (I % 5 == 0)>> final
+    : public FooBase {
+  Foo() : b{static_cast<float>(I) / 2.F} {}
+  Foo(const Foo &f) : b{f.b} { foo_copied++; }
+  Foo(Foo &&f) noexcept : b{f.b} {
+    foo_moved++;
+    f.b = -1.F;
+  }
+  ~Foo() {
+    if (b != -1.F) { // if not moved
+      foo_destructed++;
+    }
+  }
+
+  void print() const override { std::cout << std::format("Buzz: b:{}\n", b); }
+
+  float b;
+};
+
+template <std::size_t I>
+struct Foo<I, std::enable_if_t<(I % 3 != 0) && (I % 5 != 0)>> final
+    : public FooBase {
+  Foo() {}
+  Foo(const Foo &f) { foo_copied++; }
+  Foo(Foo &&f) noexcept {
+    foo_moved++;
+    f.moved = true;
+  }
+  ~Foo() {
+    if (!moved) { // if not moved
+      foo_destructed++;
+    }
+  }
+
+  void print() const override { std::cout << std::format("I: i:{}\n", I); }
+  bool moved = false;
 };
 
 template <typename Storage, std::size_t... Is>
@@ -45,10 +119,9 @@ template <typename Storage, std::size_t N> int test_storage() {
   auto anys2 = anys;
   auto anys3 = std::move(anys2);
 
-  for (auto &a : anys3) {
-    // get any Foo as Foo<0> since every Foo has same layout
-    auto *foo = a.template get<Foo<0>>();
-    std::cout << std::format("a:{}, b:{}, c:{}\n", foo->a, foo->b, foo->c);
+  for (auto const &a : anys3) {
+    auto const *foo = a.template get<FooBase>();
+    foo->print();
   }
   anys.clear();
   anys2.clear();
